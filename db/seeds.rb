0704813b1a83711f9613d4c6d9da10e8de193526
Karolina Bączyk <karolinabaczyk@o2.pl>
2021-11-09 09:36:35 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+    user = Faker::Internet.user('username', 'email')
+    User.create!(
+        name: user['username'],
+        email: user['email']
+    )
+end
+
+25.times do
+    Post.create!(
+        title: Faker::Lorem.question,
+        content: Faker::Lorem.paragraph(sentence_count: rand(5)),
+        user_id: User.order("random()").first.id
+    )
+end
